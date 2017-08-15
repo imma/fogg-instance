fd3504551f50c8534b72e845c1061507c2f14f4e
Defn Nghiem <iam@defn.sh>
2017-08-15 02:01:43 +0000

    Revert "pass less info to get remote state"
    This reverts commit 4ed94f5bf298641cbe4415ae8ba1678eba450ee8.

--- main.tf
+++ main.tf
@@ -1,10 +1,18 @@
 variable "global_bucket" {}
+variable "global_key" {}
 variable "global_region" {}
 
-variable "global_key" {}
+variable "env_bucket" {}
 variable "env_key" {}
+variable "env_region" {}
+
+variable "app_bucket" {}
 variable "app_key" {}
+variable "app_region" {}
+
+variable "service_bucket" {}
 variable "service_key" {}
+variable "service_region" {}
 
 data "terraform_remote_state" "global" {
   backend = "s3"
@@ -21,9 +29,9 @@ data "terraform_remote_state" "env" {
   backend = "s3"
 
   config {
-    bucket         = "${var.global_bucket}"
+    bucket         = "${var.env_bucket}"
     key            = "${var.env_key}"
-    region         = "${var.global_region}"
+    region         = "${var.env_region}"
     dynamodb_table = "terraform_state_lock"
   }
 }
@@ -32,9 +40,9 @@ data "terraform_remote_state" "app" {
   backend = "s3"
 
   config {
-    bucket         = "${var.global_bucket}"
+    bucket         = "${var.app_bucket}"
     key            = "${var.app_key}"
-    region         = "${var.global_region}"
+    region         = "${var.app_region}"
     dynamodb_table = "terraform_state_lock"
   }
 }
@@ -43,9 +51,9 @@ data "terraform_remote_state" "service" {
   backend = "s3"
 
   config {
-    bucket         = "${var.global_bucket}"
+    bucket         = "${var.service_bucket}"
     key            = "${var.service_key}"
-    region         = "${var.global_region}"
+    region         = "${var.service_region}"
     dynamodb_table = "terraform_state_lock"
   }
 }
