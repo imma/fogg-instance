de4dcee70201b8df9269732ddf21c32bdfb98f84
Defn Nghiem <iam@defn.sh>
2017-09-17 22:44:38 -0700

    need path vars

--- variables.tf
+++ variables.tf
@@ -24,6 +24,11 @@ variable "public_name" {
   default = ""
 }
 
+variable "remote_org_path" {}
+variable "remote_env_path" {}
+variable "remote_app_path" {}
+variable "remote_service_path" {}
+
 output "eip" {
   value = "${aws_eip.this.public_ip}"
 }
